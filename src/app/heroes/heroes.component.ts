532db77c3d2cc182c94217116413755b4967a1dd
tresqblock <german2510gus@gmail.com>
2023-02-06 11:21:29 +0200

    A few changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
-
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  heroes = HEROES;
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
   selectedHero: Hero; 
-  constructor() {}
 
-  ngOnInit() {}
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+
+
+  getHeroes(){
+    this.heroes = this.heroService.getHeroes();
+  }
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
