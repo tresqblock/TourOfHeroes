06e81913e2b5f82af501c850f51cff178816d962
tresqblock <german2510gus@gmail.com>
2023-02-09 10:58:17 +0200

    All files updated to last ver.

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,6 @@
-export class Hero {
-    id: number | undefined;
-    name: string | undefined;
+export interface Hero {
+  id: number;
+  name: string;
 }
+
+
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -11,3 +11,5 @@ export const HEROES: Hero[] = [
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
 ];
+
+
